a982d74b75b47142f1d9d93067eef520f80d7f1e
l37sg0 <petar_1v@abv.bg>
2022-12-21 20:55:17 +0200

    [#1]  - Spliting an image into pieces.

--- jquery/SlidingPuzzle/js/sliding-puzzle.js
+++ jquery/SlidingPuzzle/js/sliding-puzzle.js
@@ -22,4 +22,28 @@ $(function (){
         timer,
         currenTime      = {},
         timerDisplay    = container.find("#time").find("span");
+
+    for (var x = 0, y = aspectH; x < y; x++) {
+        for (var a = 0, b = aspectW; a < b; a++) {
+            var top = pieceH * x,
+                left = pieceW * a;
+
+            piece.clone()
+                .attr("id", idContainer++)
+                .css({
+                    width: pieceW,
+                    height: pieceH,
+                    position: "absolute",
+                    top: top,
+                    left: left,
+                    backgroundImage: ["url(", path, ")"].join(""),
+                    backgroundPosition: [
+                        "-", pieceW * a,  "px",
+                        "-", pieceH * x,  "px"
+                    ].join("")
+                }).appendTo(imgContainer);
+
+            positions.push({top: top, left: left});
+        }
+    }
 });

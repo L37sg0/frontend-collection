e6cf4afb8c72adfc9b962c471e09a39b7a20a861
l37sg0 <petar_1v@abv.bg>
2022-12-24 23:32:37 +0200

    [#12]  - Creating the page and plugin wrapper.  - Generating the underlying markup.

---
+++ jquery/FileUploader/js/uploader.js
@@ -0,0 +1,67 @@
+;(function ($) {
+    var defaults    = {
+        strings: {
+            title: "Up - A jQuery uploader",
+            dropText: "Drag files here",
+            altText: "Or select using the button",
+            buttons: {
+                choose: "Choose files",
+                upload: "Upload files"
+            },
+            tableHeadings: [
+                "Type", "Name", "Size", "Remove all x"
+            ]
+        }
+    }
+
+    function Up(el, opts) {
+        this.config = $.extend(true, {}, defaults, opts);
+        this.el     = el;
+        this.fileList   = [];
+        this.allXHR     = [];
+    }
+
+    Up.prototype.init = function () {
+        var widget  = this,
+            strings = widget.config.strings,
+            container   = $("<article/>", {
+                "class": "up"
+            }),
+            heading     = $("<header/>").appendTo(container),
+            title       = $("<h1/>", {
+                text: strings.title
+            }).appendTo(heading),
+            drop        = $("<div/>", {
+                "class": "up-drop-target",
+                html: $("<h2/>", {
+                    text: strings.dropText
+                })
+            }).appendTo(container),
+            alt         = $("<h3/>", {
+                text: strings.altText
+            }).appendTo(container),
+            upload      = $("<input/>", {
+                type: "file"
+            }).prop("multiple", true).appendTo(container),
+            select      = $("<a/>", {
+                href: "#",
+                "class": "button up-choose",
+                text: strings.buttons.choose
+            }).appendTo(container),
+            selected    = $("<div/>", {
+                "class": "up-selected"
+            }).appendTo(container),
+            upload      = $("<a/>", {
+                href: "#",
+                "class": "button up-upload",
+                text: strings.buttons.upload
+            }).appendTo(container);
+
+        widget.el.append(container);
+    }
+
+    $.fn.up = function (options) {
+        new Up(this, options).init();
+        return this;
+    };
+}(jQuery));
